1508849d084ee6501f16c5d6fb8dc9d7a3b39834
Asier Perez del Palomar <aperezd@iberdrola.es>
2025-07-11 08:23:40 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
