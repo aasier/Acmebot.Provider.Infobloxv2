02bd74ade06448fe235f524a668de249af49ccc1
Asier Perez del Palomar <aperezd@iberdrola.es>
2025-07-11 10:30:09 +0200

    update

--- Add.cs
+++ Add.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using System.Linq;
 
 namespace Acmebot.Provider.Infobloxv2
 {
