27376b58beb7f0926e4f3aa9f8548950b31eacb3
Asier Perez del Palomar <aperezd@iberdrola.es>
2025-07-11 08:52:14 +0200

    update

--- Models/DnsRequest.cs
+++ Models/DnsRequest.cs
@@ -1,7 +1,10 @@
+using System.Text.Json.Serialization;
+
 public class DnsRequest
 {
     public string Name { get; set; } = null!;
     public string? Value { get; set; }
+    [JsonPropertyName("values")]
     public string[]? Values { get; set; }
     public string Type { get; set; } = "TXT";
     public int Ttl { get; set; } = 60;
